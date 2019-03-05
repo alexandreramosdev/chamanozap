43abb2d166731cef95723ce3d3404ac43bd7c43c
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2019-03-05 01:09:32 -0300

    add feedback no button

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { withFormik, Form, Field } from "formik"
 import * as Yup from "yup"
 import Clipboard from "react-clipboard.js"
@@ -7,49 +7,53 @@ import SelectCountries from "./selectCountries"
 
 const FInput = ({ field, ...props }) => <Input {...field} {...props} />
 
-const InputFormik = ({ errors, touched, values, handleChange, handleBlur }) => (
-  <Form>
-    <div style={{ padding: "5px" }}>
-      <p style={{ color: "gray" }}>Digite o n° ( Ex. 11 97718-5120 )</p>
-      <SelectCountries
-        value={values.countries}
-        name="countries"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        className="ui search selection dropdown"
-        style={{ display: "block", margin: "0 auto 5px" }}
-      />
-      <Field
-        size="huge"
-        name="tel"
-        type="tel"
-        component={FInput}
-        placeholder="11 97718-5120"
-      />
-      {errors.tel && touched.tel && (
-        <p style={{ color: "red" }}>{errors.tel}</p>
-      )}
-    </div>
+const InputFormik = ({ errors, touched, values, handleChange, handleBlur }) => {
+  const [active, setActive] = useState(false)
+  return (
+    <Form>
+      <div style={{ padding: "5px" }}>
+        <p style={{ color: "gray" }}>Digite o n° ( Ex. 11 97718-5120 )</p>
+        <SelectCountries
+          value={values.countries}
+          name="countries"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className="ui search selection dropdown"
+          style={{ display: "block", margin: "0 auto 5px" }}
+        />
+        <Field
+          size="huge"
+          name="tel"
+          type="tel"
+          component={FInput}
+          placeholder="11 97718-5120"
+        />
+        {errors.tel && touched.tel && (
+          <p style={{ color: "red" }}>{errors.tel}</p>
+        )}
+      </div>
 
-    <Button.Group size="big">
-      <Button positive type="submit" disabled={!errors.tel && !values.tel}>
-        Chamar
-      </Button>
-      <div className="or" data-text="ou" />
-      <Clipboard
-        className={`${
-          !errors.tel && !values.tel ? "ui button disabled" : "ui button"
-        }`}
-        disabled={!values.tel}
-        onSuccess={() => console.info("Link copiado com sucesso!")}
-        data-clipboard-text={`https://wa.me/${values.tel}`}
-        button-title="Copie o link e compatilhe "
-      >
-        Copie link
-      </Clipboard>
-    </Button.Group>
-  </Form>
-)
+      <Button.Group size="big">
+        <Button positive type="submit" disabled={!errors.tel && !values.tel}>
+          Chamar
+        </Button>
+        <div className="or" data-text="ou" />
+        <Clipboard
+          className={`${
+            (!errors.tel && !values.tel) || active
+              ? "ui button disabled"
+              : "ui button"
+          }`}
+          onSuccess={() => setActive(true)}
+          data-clipboard-text={`https://wa.me/${values.countries}${values.tel}`}
+          button-title="Copie o link e compatilhe "
+        >
+          {active ? "link Copiado" : "Copie link"}
+        </Clipboard>
+      </Button.Group>
+    </Form>
+  )
+}
 
 const phoneRegExp = /^[0-9]{2,3}[0-9]{4,5}[0-9]{4,5}$/
 
@@ -64,8 +68,7 @@ const Formik = withFormik({
     tel: Yup.string().matches(phoneRegExp, "Numero de telefone invalido"),
   }),
   handleSubmit: ({ tel, countries }) => {
-    console.log(`Countries:`, countries, `Tel`, tel)
-    // window.open(`https://wa.me/${countries}${tel}`)
+    window.open(`https://wa.me/${countries}${tel}`)
   },
 })(InputFormik)
 
