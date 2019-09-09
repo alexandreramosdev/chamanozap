df28d454ce95a8fccba7d1a9d7b7a456519db8aa
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2019-09-09 05:20:40 -0300

    add install ios

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,3 +9,17 @@ exports.onServiceWorkerUpdateReady = () => {
     window.location.reload()
   }
 }
+
+const gWindow = typeof window !== 'undefined' && window
+
+// Detecta se o dispositivo está no iOS
+const isIos = () => {
+  const userAgent = gWindow.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in gWindow.navigator) && (gWindow.navigator.standalone);
+// Verifica se deve exibir notificação popup de instalação:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+}
