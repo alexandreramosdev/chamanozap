db7853d0ac5c702b030df3b07dd1a42dad6bb2a9
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2019-09-09 05:38:17 -0300

    Fix install ios

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,16 +10,19 @@ exports.onServiceWorkerUpdateReady = () => {
   }
 }
 
-const gWindow = typeof window !== 'undefined' && window
+exports.onInitialClientRender = () => {  
+  // Detecta se o dispositivo está no iOS
+  const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test( userAgent );
+  }
+  // Detects if device is in standalone mode
+  const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+  // Verifica se deve exibir notificação popup de instalação:
+  if (isIos() && !isInStandaloneMode()) {
+    this.setState({ showInstallMessage: true });
+  }
+}
 
-// Detecta se o dispositivo está no iOS
-const isIos = () => {
-  const userAgent = gWindow.navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test( userAgent );
-}
-// Detects if device is in standalone mode
-const isInStandaloneMode = () => ('standalone' in gWindow.navigator) && (gWindow.navigator.standalone);
-// Verifica se deve exibir notificação popup de instalação:
-if (isIos() && !isInStandaloneMode()) {
-  this.setState({ showInstallMessage: true });
-}
+
+
